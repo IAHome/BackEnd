61718610c13659a5b87ab1fe7f7c567761b86bff
KleyverCode <89604760+KleyverCode@users.noreply.github.com>
2023-10-22 20:20:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,18 @@ comando_instalacion = ["pip", "install", "flask"]
 subprocess.run(comando_instalacion)
 
 from flask import Flask, request, jsonify
-from modelo import Modelo
 
 app = Flask(__name__)
 CORS(app)
-modelo_instancia = Modelo()
+
+from modelo import predecir_cluster
 
 @app.route('/agrupar', methods=['POST'])
-
-def agrupar():
-    data = request.get_json()
-    parametros = ['edad', 'genero', 'comuna', 'tipoVivienda', 'totalPersonasH', 'sumaIngresosAux','sumaIngresosLAV', 'sumaIngresosExt', 'sumaGastos']
-    for param in parametros:
-        if param not in data:
-            return jsonify({'error': f'Falta el parámetro {param}'}), 400
-
-    grupo = modelo_instancia.agrupar(**data)
-    
-
-    return jsonify({'grupo': grupo})
+def predecir():
+    datos_nuevo_registro = request.get_json()  # Suponiendo que los datos se envían como JSON desde la solicitud POST
+    cluster_predicho = predecir_cluster(datos_nuevo_registro)
+    return jsonify({'cluster_predicho': cluster_predicho})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
